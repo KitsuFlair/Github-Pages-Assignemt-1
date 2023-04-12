05a0062662292b1cc6abee982776be0068eb76b0
Kiernan Quarterman <71052150+KitsuFlair@users.noreply.github.com>
2023-04-11 20:49:04 -0400

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,18 @@
 async function getData(chosen_major) {
-    var response = await fetch('cit5students.json');   
+    var response = await fetch('cit5students.json');
 
     if(response.ok) {
         var data = await response.json();
 
-        majors = data.filter( (item) => item.major == chosen_major );    
+        majors = data.filter( (item) => item.major == chosen_major );
             
        var templateText = document.getElementById('majorsTemplate').innerHTML;
-       var compiledTemplateText = Handlebars.compile(templateText);   
-       compiledHtml = compiledTemplateText({ rows: majors })      
+       var compiledTemplateText = Handlebars.compile(templateText);
+       compiledHtml = compiledTemplateText({ rows: majors });
        document.getElementById('majorsResults').innerHTML = compiledHtml; 
    }
    else {
-       document.querySelector('#majorsResults').innerHTML = "There was an error, or major not found";
+       document.querySelector('#majorsResults').innerHTML = "There was an error, or menu items not found";
    }	
 
 }
