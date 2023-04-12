d7eb78409a74f4864f363350eb1498954a8bb3ba
Kiernan Quarterman <71052150+KitsuFlair@users.noreply.github.com>
2023-04-11 20:46:08 -0400

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,18 @@
 async function getData(chosen_major) {
-    var response = await fetch('cit5students.json');   // this is an ajax GET request
+    var response = await fetch('cit5students.json');   
 
     if(response.ok) {
         var data = await response.json();
 
-        majors = data.filter( (item) => item.major == chosen_major );    // filter data array for selected meal items 
+        majors = data.filter( (item) => item.major == chosen_major );    
             
        var templateText = document.getElementById('majorsTemplate').innerHTML;
-       var compiledTemplateText = Handlebars.compile(templateText);   // get and compile template code
-       compiledHtml = compiledTemplateText({ rows: majors })      // apply data to template
+       var compiledTemplateText = Handlebars.compile(templateText);   
+       compiledHtml = compiledTemplateText({ rows: majors })      
        document.getElementById('majorsResults').innerHTML = compiledHtml; 
    }
    else {
-       document.querySelector('#majorsResults').innerHTML = "There was an error, or menu items not found";
+       document.querySelector('#majorsResults').innerHTML = "There was an error, or major not found";
    }	
 
 }
